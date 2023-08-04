14998429b227c41aef61133abdcb347ea59d99bd
Umang_Varotariya <umangvarotariya@gmail.com>
2023-08-04 16:23:42 +0530

    Create Moveforward and ExecuteCommand function . Testing of this function

--- Chandrayan3.js
+++ Chandrayan3.js
@@ -17,6 +17,95 @@ class Chandrayan3_LunarCraft {
         this.currDirection = currDirection
         this.galaxy = galaxy
     }
+
+    moveForward() {
+        if (this.currDirection == 'N') {
+            if (this.y + 1 <= this.galaxy.py) {
+                this.y += 1;
+            }
+            else {
+                throw Error("You have reached Max Y-boundary of galaxy");
+            }
+        }
+        else if (this.currDirection == 'S') {
+            if (this.y - 1 >= this.galaxy.ny) {
+                this.y -= 1;
+            }
+            else {
+                throw Error("You have reached Max Y-boundary of galaxy");
+            }
+        }
+        else if (this.currDirection == 'E') {
+            if (this.x + 1 <= this.galaxy.px) {
+                this.x += 1;
+            }
+            else {
+                throw Error("You have reached Max X-boundary of galaxy");
+            }
+        }
+        else if (this.currDirection == 'W') {
+            if (this.x - 1 >= this.galaxy.nx) {
+                this.x -= 1;
+            }
+            else {
+                throw Error("You have reached Max X-boundary of galaxy");
+            }
+        }
+        else if (this.currDirection == 'U') {
+            if (this.z + 1 <= this.galaxy.pz) {
+                this.z += 1;
+            }
+            else {
+                throw Error("You have reached Max Z-boundary of galaxy");
+            }
+        }
+        else if (this.currDirection == 'D') {
+            if (this.z - 1 >= this.galaxy.nz) {
+                this.z -= 1;
+            }
+            else {
+                throw Error("You have reached Max Z-boundary of galaxy");
+            }
+        }
+    }
+
+    moveBackward(){}
+
+    moveLeft(){}
+
+    moveRight(){}
+
+    moveUpward(){}
+
+    moveDownward(){}
+
+    executeCommands(command) {
+        for (const cmd of command) {
+            switch (cmd) {
+                case 'f':
+                    this.moveForward()
+                    break;
+                case 'b':
+                    this.moveBackward()
+                    break;
+                case 'l':
+                    this.moveLeft()
+                    break;
+                case 'r':
+                    this.moveRight()
+                    break;
+                case 'u':
+                    this.moveUpward()
+                    break;
+                case 'd':
+                    this.moveDownward()
+                    break;
+                default:
+                    throw Error("Invalid Command")
+                    break;
+            }
+        }
+    }
 }
 
 module.exports = {
--- solution.spec.js
+++ solution.spec.js
@@ -20,4 +20,43 @@ describe("Initializa the state of Chandrayan3_LunarCraft class and Galaxy class"
         expect(chandrayan.galaxy.ny).toBe(-3);
         expect(chandrayan.galaxy.nz).toBe(-3);
     })
+});
+
+describe("Testing Classes Functions",()=>{
+    let galaxy;
+    let chandrayan;
+    beforeEach(()=>{
+        galaxy = new Galaxy(3,3,3,3,3,3);
+        chandrayan = new Chandrayan3_LunarCraft(0,0,0,'N',galaxy);
+    })
+    test("Testing for moveForward() Function with correct output",()=>{
+        command = ['f','f']
+        chandrayan.executeCommands(command);
+        chandrayan.moveForward();
+        expect(chandrayan.x).toBe(0);
+        expect(chandrayan.y).toBe(3);
+        expect(chandrayan.z).toBe(0);
+    })
+    test("Testing for moveForward() Function with error",()=>{
+        command = ['f','f','f','f']
+        try{
+            
+            chandrayan.executeCommands(command);
+            chandrayan.moveForward();
+        }
+        catch(e){
+            expect(e.message).toEqual("You have reached Max Y-boundary of galaxy")
+        }
+    })
+    test("Testing for executeCommand Function with error",()=>{
+        command = ['z','p']
+        try{
+            
+            chandrayan.executeCommands(command);
+        }
+        catch(e){
+            expect(e.message).toEqual("Invalid Command")
+        }
+    })
+    
 })
